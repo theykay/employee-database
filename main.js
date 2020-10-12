88ef8754ace10751e8d89236ca70cd09aa0bbca6
TheyKay <murraykyleb@gmail.com>
2020-10-11 20:42:04 -0600

    added variables for chalk colors

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const chalk = require("chalk");
+const table = require("table");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -10,6 +11,15 @@ var connection = mysql.createConnection({
     password: process.env.mysql,
     database: "employees_db"
 })
+
+// console colors
+const red = chalk.bold.red;
+const green = chalk.bold.green;
+const yellow = chalk.bold.yellow;
+const blue = chalk.bold.yellow;
+const magenta = chalk.bold.magenta;
+const cyan = chalk.bold.cyan;
+const grey = chalk.bold.grey;
 
 connection.connect(function(err) {
     if (err) {
